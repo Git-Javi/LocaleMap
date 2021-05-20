ec594735dcf66861d4d60dbb6af649220d4e8fa3
Git-Javi <pruebasfpcastelao@gmail.com>
2021-05-20 23:49:15 +0200

    Testeo de servicio

--- localemap-back/src/main/java/gz/localemap/app/controller/LocalController.java
+++ localemap-back/src/main/java/gz/localemap/app/controller/LocalController.java
@@ -2,8 +2,13 @@ package gz.localemap.app.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import gz.localemap.app.entity.Local;
@@ -11,16 +16,18 @@ import gz.localemap.app.service.LocalService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Validated
 @Slf4j
 public class LocalController {
 	
 	@Autowired
 	private LocalService localService;
 
-	@GetMapping(value = "/locales")
-	public List<Local> test() {	
-		List <Local> locales = localService.getAllLocalByCategoria("Restaurantes");
-		locales.stream().forEach(local->{log.info("Local....."+local);});
+	@GetMapping(value = "/locales/{categoria}")
+	public List<Local> getAllLocalByCategoria(@PathVariable("categoria") @Valid @NotEmpty String categoria) {	
+		log.info("LocalController :: categoria ===> "+categoria);
+		List <Local> locales = localService.getAllLocalByCategoria(categoria.trim());
+		locales.stream().forEach(local->{log.info("LocalController :: Locales ===> "+local);});
 		return locales;
 	}
 
--- localemap-back/src/main/java/gz/localemap/app/service/LocalServiceImpl.java
+++ localemap-back/src/main/java/gz/localemap/app/service/LocalServiceImpl.java
@@ -2,21 +2,30 @@ package gz.localemap.app.service;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import gz.localemap.app.entity.Local;
 import gz.localemap.app.repository.LocalRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Validated
+@Slf4j
 public class LocalServiceImpl implements LocalService {
 	
 	@Autowired
 	private LocalRepository localRepository;
 	
 	@Override
-	public List<Local> getAllLocalByCategoria(String categoria) {
+	public List<Local> getAllLocalByCategoria(@Valid @NotEmpty String categoria) {
+		log.info("LocalServiceImpl :: categoria ===> "+categoria);
 		List<Local> locales = localRepository.getAllLocalByCategoria(categoria);
+		locales.stream().forEach(local->{log.info("LocalServiceImpl :: Locales ===> "+local);});
 		return locales;
 	}
 
